8dacbc330637760cc0bd65239848b633100f5752
mued <almued101@gmail.com>
2022-08-29 21:08:56 +0600

    final commit

--- app/src/main/java/com/example/newsdaily/Service/Model/Article.java
+++ app/src/main/java/com/example/newsdaily/Service/Model/Article.java
@@ -1,24 +1,43 @@
 package com.example.newsdaily.Service.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class Article implements Serializable {
 
 
+    @SerializedName("source")
+    @Expose
     private Source source;
 
+    @SerializedName("author")
+    @Expose
     private String author;
 
+    @SerializedName("title")
+    @Expose
     private String title;
 
+    @SerializedName("description")
+    @Expose
     private String description;
 
+    @SerializedName("url")
+    @Expose
     private String url;
 
+    @SerializedName("urlToImage")
+    @Expose
     private String urlToImage;
 
+    @SerializedName("publishedAt")
+    @Expose
     private String publishedAt;
 
+    @SerializedName("content")
+    @Expose
     private String content;
 
     public Source getSource() {
